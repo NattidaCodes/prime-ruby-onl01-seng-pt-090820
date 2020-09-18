5341ed01d53d61ab0e3b2b90331e4a361fa1b9ca
Nattida Wong <nattidac.wong@gmail.com>
2020-09-18 21:49:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(num)
   if num < 2
     false
   else
-    
+
     # if num is divisible by a number other than 1 of itself
     [2..num - 1].to_a.all do |n|
       num % n != 0 #evaluating if num divisible (prime)
